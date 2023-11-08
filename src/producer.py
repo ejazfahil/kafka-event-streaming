db360c37c14bb8c9bc8055b10ee9d4815a527cd6
Fahil Ejaz <ejazfahil@gmail.com>
2023-11-08 11:45:00 +0100

    feat: add typed Kafka producer

---
+++ src/producer.py
@@ -0,0 +1,28 @@
+"""Kafka producer 2023-11-08"""
+import json, uuid
+from datetime import datetime, timezone
+from confluent_kafka import Producer
+from dataclasses import dataclass, asdict
+
+@dataclass
+class PaymentEvent:
+    event_id: str
+    user_id: str
+    amount: float
+    currency: str
+    status: str
+    created_at: str
+
+def make_producer(brokers="localhost:9092"):
+    return Producer({"bootstrap.servers": brokers,
+                     "acks": "all",
+                     "enable.idempotence": True,
+                     "compression.type": "snappy"})
+
+def emit(producer: Producer, topic: str, event: PaymentEvent):
+    producer.produce(
+        topic, key=event.user_id.encode(),
+        value=json.dumps(asdict(event)).encode(),
+        callback=lambda err, msg: print(f"[ERROR] {err}" if err else f"[OK] {msg.offset()}")
+    )
+    producer.poll(0)
